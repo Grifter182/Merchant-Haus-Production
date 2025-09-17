a7026958d07c1ef4b98670a31197eae991f22af3
darrynrooza-stack <darryn.rooza@nmi.com>
2025-09-17 05:50:35 -0500

    Add Squarespace integration branding

--- index.php
+++ index.php
@@ -114,6 +114,16 @@
           ],
         ],
         [
+          'name' => 'Squarespace',
+          'href' => 'https://www.squarespace.com/',
+          'target' => '_blank',
+          'rel' => 'noopener',
+          'logos' => [
+            'light' => mh_asset('public/assets/images/squarespace-light.svg'),
+            'dark' => mh_asset('public/assets/images/squarespace-dark.svg'),
+          ],
+        ],
+        [
           'name' => 'GoHighLevel',
           'href' => 'gohighlevel.html',
           'logos' => [
